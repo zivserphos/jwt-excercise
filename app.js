55a44e5940fef702b9690fa7c09b1d4b0ee526f3
zivserphos <zivfromisrael@gmail.com>
2021-11-30 18:30:07 +0200

    added  register functionllity

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const morganHandler = require("./middlewares/morgan");
 const errorHandler = require("./middlewares/errorHandlers");
 const app = express();
 
+const SECRET =
+  "d5bf5d20d36046da2c4f6dc16ad03c4143adf399cb4bc1fd25dc03462ac951e946116e632af0a10127900437972c63cf622a1b6401c30e5134c44e2c3f8dc698";
+
 const USERS = [];
 const INFORMATION = [];
 const REFRESHTOKENS = [];
@@ -21,29 +24,45 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/v1/information");
-app.get("/api/v1/users");
+app.get("/api/v1/users", (req, res) => {});
 
 app.post("/users/register", async (req, res) => {
-  const { email, user, password } = req.body;
+  console.log(USERS);
+  const { email, name, password } = req.body;
   if (
     USERS.findIndex((user) => {
-      user.name === user;
+      user.name === name;
     }) !== -1
   )
     return res.status(409).send("user already exists");
   const hashPassword = await bcrypt.hash(password, await bcrypt.genSalt(10));
   USERS.push({
     email: email,
-    name: user,
+    name: name,
     password: hashPassword,
     isAdmin: false,
   });
   console.log(hashPassword);
-  INFORMATION.push({ email: `${email}`, info: `${user} info` });
+  INFORMATION.push({ email: `${email}`, info: `${name} info` });
   res.status(201).send("Register Success");
 });
 
-app.post("/users/login");
+app.post("/users/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = USERS.find((user) => user.email === email);
+  if (!user) return res.status(404).send("cannot find user");
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(403).send("User or Password incorrect");
+  const token = jwt.sign(user, SECRET, { expiresIn: "10s" });
+  const lastToken = jwt.sign(user, SECRET);
+  res.status(200).send({
+    accessToken: token,
+    refreshToken: lastToken,
+    email: email,
+    name: user.name,
+    isAdmin: false,
+  });
+});
 
 app.post("/users/token");
 
--- controllers/userController.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcrypt");
-const USERS = require("USER");
-
-exports.register = async (req, res) => {
-  const registerData = req.body;
-  res.status(201).send("Register Success");
-};
